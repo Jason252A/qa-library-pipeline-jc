18763c58001ec3fb9602bd41a0f9efcecf8dad28
Jason <Jason.Cheung@Cabotfinancial.com>
2026-06-30 12:16:22 +0100

    add some code and all sorts

--- src/data_processing/validation.py
+++ src/data_processing/validation.py
@@ -6,4 +6,24 @@ Data validation functions.
 # Example function to implement:
 def validate_isbn(isbn):
     """Validate ISBN-13 format."""
-    return True
+    isbn = str(isbn)
+    isbn = isbn.replace("-","")
+#    isbntxt = isbntxt.replace("-","")
+#    try:
+#        int(isbn)
+#        print("pass: " + isbntxt)
+#        if len(a) == 13
+#        return True
+#        else
+#            return False
+#    except:
+#        print("fail: " + str(isbn))      
+#        return False
+    if len(isbn) == 13:
+        return True
+    else:
+        print("Fail: " + isbn)
+        return False
+
+
+#    return True
